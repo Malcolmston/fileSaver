28d40125c9588d05680062858eaa1b2bea3dfed4
alcolmston <mstone@crefeld.org>
2024-03-20 19:33:41 -0400

    packages for node
    1. sequelize
    2. byte-size
    3. bcrypt
    4. fs
    
    npm install sequelize sqlite3 byte-size bcrypt fs

--- userManegement.js
+++ userManegement.js
@@ -7,4 +7,14 @@ const salt = process.env.SALT;
 if (!salt) {
     console.error("Error: PORT, HOST, or SALT environment variables are missing.");
     process.exit(1);
+<<<<<<< 1441a9afc1ee25c27633a7d1e81e14df7f3525e7
 }
+=======
+}
+
+const { Sequelize, DataTypes, Op } = require('sequelize');
+
+const byteSize = require('byte-size')
+const bcrypt = require("bcrypt");
+const fs = require('fs');
+>>>>>>> packages for node
