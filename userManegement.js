a2a68b6eea83de555226e09bf2c5f36c523fe505
alcolmston <mstone@crefeld.org>
2024-03-21 17:29:05 -0400

    validate server
    even with a local SQLlite server, checking its existence is still good.

--- userManegement.js
+++ userManegement.js
@@ -7,14 +7,22 @@ const salt = process.env.SALT;
 if (!salt) {
     console.error("Error: PORT, HOST, or SALT environment variables are missing.");
     process.exit(1);
-<<<<<<< 1441a9afc1ee25c27633a7d1e81e14df7f3525e7
 }
-=======
-}
+
+
 
 const { Sequelize, DataTypes, Op } = require('sequelize');
 
 const byteSize = require('byte-size')
 const bcrypt = require("bcrypt");
 const fs = require('fs');
->>>>>>> packages for node
+
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
