6ff1487228dd096b73478c500421edea0b60f5ca
alcolmston <mstone@crefeld.org>
2024-03-23 22:53:50 -0400

    gets the SECRET key from the enviroment

--- router.js
+++ router.js
@@ -4,3 +4,6 @@ const { Basic, Admin, File } = require('./userManegement.js');
 const byteSize = require('byte-size')
 const fs = require('fs');
 const session = require('express-session')
+
+
+const {SECRET} =  process.env;
