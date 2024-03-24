0e09b09b46e2735f46444d78a7ea823fc80af529
alcolmston <mstone@crefeld.org>
2024-03-23 22:58:58 -0400

    import userManegement

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config() // adds the ability to create environment variables
 const express = require('express');
 const multer = require('multer');
 const {sessionMiddleware} = require('./router.js')
+const { Basic, Admin, File } = require('./userManegement.js');
 
 const app = express();
 
@@ -18,6 +19,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('scripts'))
 app.use(express.static('styles'))
 app.use(sessionMiddleware);
+
+app.get("/", async (req, res, next) => {
+    const { valid, username, isAdmin } = req.session
+    if (isAdmin && valid) {
+      let data = await Admin.getAll(username);
+  
+  
+    } else if (valid) {
+      res.render('basic', { username, message: '' });
+    } else {
+      res.render('home', { message: "" });
+    }
+  
+  
+  })
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
