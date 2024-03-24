ead8636f51064b6aa55318ccd141d3eeb6d57448
alcolmston <mstone@crefeld.org>
2024-03-23 22:56:42 -0400

    app engines allows for forms, files, and ejs

--- server.js
+++ server.js
@@ -8,5 +8,17 @@ const app = express();
 
 const {PORT,HOST} = process.env;
 
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('scripts'))
+app.use(express.static('styles'))
+
+
+app.use(sessionMiddleware);
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
